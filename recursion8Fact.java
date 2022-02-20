df3dca3840a73493bae348f76e27af0566ba80ac
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-21 00:03:54 +0530

    Create recursion8Fact.java

---
+++ recursion8Fact.java
@@ -0,0 +1,21 @@
+package com.basicsinjava;
+
+public class Main {
+    public static void printSum(int i, int n, int sum){
+        if(i==n){
+            sum=sum*i;
+            System.out.println(sum);
+            return;
+        }
+        if(n==0){
+            System.out.println("1");
+            return;
+        }
+        sum = sum*i;
+        printSum(i+1, n, sum);
+    }
+    public static void main(String[] args) {
+        int n = 112;
+        printSum(1, n, 1);
+    }
+}
