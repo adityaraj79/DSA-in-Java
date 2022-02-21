f79afe48a427d818ba28a2b52fb41852440d2f08
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-02-21 18:37:55 +0530

    Create recursion10Fib.java

---
+++ recursion10Fib.java
@@ -0,0 +1,20 @@
+package com.basicsinjava;
+
+public class Main {
+    public static void calcFib(int a, int b, int n) {
+        if(n==0){
+            return;
+        }
+        int c = (a+b);
+        System.out.println(c);
+        calcFib(b, c, n-1);
+    }
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 9;
+        calcFib(a, b, n-2);
+    }
+}
