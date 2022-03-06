44c391fab40caf0d41001cc3f60301be2f5aad13
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-03-06 23:47:03 +0530

    Create recursion18removeDuplicates.java

---
+++ recursion18removeDuplicates.java
@@ -0,0 +1,25 @@
+package com.basicsinjava;
+
+public class Main {
+    public static boolean map[]= new boolean[26];
+    public static void removeDuplicates(String str, int idx, String newString){
+        if(idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a']==true){
+            removeDuplicates(str, idx+1, newString);
+        }else{
+            newString += currChar;
+            map[currChar-'a'] = true;
+            removeDuplicates(str, idx+1, newString);
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "axabxxbbxcxd";
+        removeDuplicates(str, 0, "");
+    }    
+}
